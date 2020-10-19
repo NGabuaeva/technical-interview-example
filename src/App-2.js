e346efd4a80332246ad6b7a4395efcab8e300d6b
Alexander Wu <alexanderwu2000@gmail.com>
2020-10-19 13:50:20 +0200

    Add second problem

---
+++ src/App-2.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const data = [
+  {
+    id: 1,
+    label: "Day One",
+    times: [23],
+  },
+  {
+    id: 2,
+    label: "Day Two",
+    times: [2, 4, 18, 20, 23],
+  },
+  {
+    id: 3,
+    label: "Day Three",
+    times: [2, 4, 18, 20, 23],
+  },
+  {
+    id: 4,
+    label: "Day Four",
+    times: [2, 4, 18, 20],
+  },
+  {
+    id: 5,
+    label: "Day Five",
+    times: [17, 19, 22],
+  },
+  {
+    id: 6,
+    label: "Day Six",
+    times: [1, 17, 19, 22],
+  },
+];
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
